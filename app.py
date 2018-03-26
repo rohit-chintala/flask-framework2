5048c1dff410a2d2e37b648425163b826a90747e
Rohit Chintala <rohit.chintala@gmail.com>
2018-03-26 09:41:20 -0500

    Test2

--- app.py
+++ app.py
@@ -28,18 +28,22 @@ def index():
         #ticker_feature = request.form['features']
         stock_data = requests.get('https://www.quandl.com/api/v3/datasets/WIKI/%s.json?api_key=Y4tZ_xJ8saiyQVoUEQEP'%ticker_symbol)
         stock_data_dict = stock_data.json()
-        column_names = stock_data_dict['dataset']['column_names']
-        stock_df = pd.DataFrame(stock_data_dict['dataset']['data'],columns = column_names)
-        stock_lm = stock_df[stock_df.last_valid_index() - 29:]
-        close_data = stock_lm.Close
-        p1 = bokehfigure()
-        p1.grid.grid_line_alpha = 0.3
-        p1.xaxis.axis_label = 'Days'
-        p1.yaxis.axis_label = 'Price'
-        xdata = range(1,31,1)
-        p1.line(xdata,close_data,color = '#A6CEE3',legend = ticker_symbol)
-        script,div = components(p1)
-        return render_template('figure.html',script = script, div = div)
+        ticker_test = [*stock_data_dict]
+        if ticker_test == ['quandl_error']:
+            return render_template('error.html')
+        else:
+            column_names = stock_data_dict['dataset']['column_names']
+            stock_df = pd.DataFrame(stock_data_dict['dataset']['data'],columns = column_names)
+            stock_lm = stock_df[stock_df.last_valid_index() - 29:]
+            close_data = stock_lm.Close
+            p1 = bokehfigure()
+            p1.grid.grid_line_alpha = 0.3
+            p1.xaxis.axis_label = 'Days'
+            p1.yaxis.axis_label = 'Price'
+            xdata = range(1,31,1)
+            p1.line(xdata,close_data,color = '#A6CEE3',legend = ticker_symbol)
+            script,div = components(p1)
+            return render_template('figure.html',script = script, div = div)
 
 
 if __name__ == '__main__':
